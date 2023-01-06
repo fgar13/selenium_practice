764cfb84522351e141f89a72339516ea7e5dd409
fgar13 <94147771+fgar13@users.noreply.github.com>
2023-01-05 22:20:53 -0500

    Search for 'Software' jobs on Indeed

--- selenium_practice.py
+++ selenium_practice.py
@@ -1,7 +1,20 @@
 from selenium import webdriver as wd
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 driver = wd.Chrome() #or Firefox()
-
 driver.get('https://www.indeed.com/')
-time.sleep(5000)
+
+input_what = driver.find_element(By.ID, "text-input-what")
+input_what.click()
+input_what.send_keys('Software')
+input_what.send_keys(Keys.ENTER)
+
+time.sleep(5)
+#NOTES:
+#driver.find_element_by_css_selector() - is also an option, when looking for a css class
+#in the case of Indeed.com's what and where, I could've looked for
+#class = "icl-TextInput-control icl-TextInput-control--withIconRight"
+
+#driver.find_element_by_xpath()
